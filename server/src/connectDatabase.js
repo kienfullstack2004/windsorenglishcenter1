9757960190bacedb4d0580899e7dc7b32d688720
kienfullstack2004 <kiennguyenfullstack2004@gmail.com>
2024-09-22 23:03:03 +0700

    update2

--- server/src/connectDatabase.js
+++ server/src/connectDatabase.js
@@ -1,19 +1,22 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
+//
+//  
+//
 const sequelize = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  logging: false
 });
 
-const connect = async() => {
-    try {
-        await sequelize.authenticate();
-        console.log('Kết nối tới database thành công.');
-      } catch (error) {
-        console.error('Kết nối với database thất bại :', error);
-      }
+const connect = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Kết nối tới database thành công.');
+  } catch (error) {
+    console.error('Kết nối với database thất bại :', error);
+  }
 }
 
 connect();
