d61eab328c09e180ac529c9ce0ff8693445cde87
kienfullstack2004 <kiennguyenfullstack2004@gmail.com>
2024-09-22 22:50:35 +0700

    update second

--- server/src/connectDatabase.js
+++ server/src/connectDatabase.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('windsorcenter', 'root', null, {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     logging: false
 });
